ca0b31e28d1ac2c3165f0773558dfb8721262bd3
andirb22 <35705561+andirb22@users.noreply.github.com>
2022-09-22 16:40:34 -0500

    Revert "modificacion de los archivos dockerfile y docker-compose"
    This reverts commit 7ac7bb7847c571b6fa7e6ad5a7af1a5747d2ad4c.

--- src/app.py
+++ src/app.py
@@ -6,10 +6,13 @@ from controllers import Books_Controller
 from controllers import error_handler
 from controllers.error_handler import create_flask_app_with_error_handlers
 
+
+
+
 app = create_flask_app_with_error_handlers()
 app.register_blueprint(Books_Controller.BOOKS_CONTROLLER)
 
 if __name__=="__main__":
     create_tables()
-    app.run(debug=True, port=4000)
+    app.run(debug=True,port=4000)
     
--- src/models/configDB.py
+++ src/models/configDB.py
@@ -1,11 +1,10 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASEURI')
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:12345678@localhost/flaskmysql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SQLALCHEMY_POOL_RECYCLE']=3600
 
